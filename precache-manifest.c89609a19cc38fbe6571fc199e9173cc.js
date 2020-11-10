61b658cb8a5a3c49243ccb5813be9e47eb94bc29
KANGDONGHWI <rkdehdgnl@naver.com>
2020-11-10 22:33:32 +0900

    Updates

---
+++ precache-manifest.c89609a19cc38fbe6571fc199e9173cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652e5c2c941ee1ec6ded58dc541d6466",
+    "url": "/dongplix-hooks/index.html"
+  },
+  {
+    "revision": "ae53379eea85f3444c1b",
+    "url": "/dongplix-hooks/static/js/2.d245b04b.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/dongplix-hooks/static/js/2.d245b04b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a730802c83616533bcf0",
+    "url": "/dongplix-hooks/static/js/main.dbbd9ca0.chunk.js"
+  },
+  {
+    "revision": "d2a39eafbe661509cac0",
+    "url": "/dongplix-hooks/static/js/runtime-main.9959850a.js"
+  },
+  {
+    "revision": "6bc11c2ea87b959eb371dd2e91f4fb5b",
+    "url": "/dongplix-hooks/static/media/Loading.6bc11c2e.gif"
+  },
+  {
+    "revision": "13526e1eb815abe5be9b3acb01d9dccc",
+    "url": "/dongplix-hooks/static/media/NoVideo.13526e1e.jpg"
+  },
+  {
+    "revision": "eac42b8d229b02713f3b457204f27309",
+    "url": "/dongplix-hooks/static/media/header.eac42b8d.jpg"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/dongplix-hooks/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
